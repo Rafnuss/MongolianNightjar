c17edbe38269172c0cbaf3a20a1cfa36910721be
Raphaël Nussbaumer <rafnuss@gmail.com>
2025-08-16 08:23:02 +0200

    minor edits

--- analysis/5-datapackage.R
+++ analysis/5-datapackage.R
@@ -2,46 +2,17 @@ library(GeoLocatoR)
 library(zen4R)
 library(frictionless)
 
+zenodo <- ZenodoManager$new(token = keyring::key_get(service = "ZENODO_PAT"))
+
+
 ## Publish Data Package
 # Introduction: hhttps://raphaelnussbaumer.com/GeoPressureManual/geolocator-intro.html
 # Detailed instruction: https://raphaelnussbaumer.com/GeoPressureManual/geolocator-create.html
 
 # Create the datapackage
-pkg <- create_gldp_geopressuretemplate(".")
-
-#################
-# Create Metadata
-
-# Contributors/creators:
-# Default is to take the GeoPressureTemplate authors, but it is common that
-# additional co-authors should be added for the datapackage
-pkg$contributors <- list( # required
-  list(
-    title = "Raphaël Nussbaumer",
-    roles = c("ContactPerson", "DataCurator", "ProjectLeader"),
-    email = "raphael.nussbaumer@vogelwarte.ch",
-    path = "https://orcid.org/0000-0002-8185-1020",
-    organization = "Swiss Ornithological Institute"
-  )
-)
-
-
-# Related Identifiers
-# e.g. papers, project pages, derived datasets, etc.
-pkg$relatedIdentifiers <- list(
-  list(
-    relationType = "IsSupplementTo",
-    relatedIdentifier = "10.1007/s10336-022-02000-4",
-    relatedIdentifierType = "DOI"
-  ),
-  list(
-    relationType = "IsDerivedFrom",
-    relatedIdentifier = "https://github.com/Rafnuss/MongolianNightjar",
-    relatedIdentifierType = "URL"
-  )
-)
-
-# print(pkg)
+# pkg <- create_gldp_geopressuretemplate(".")
+z <- zenodo$getDepositionByConceptDOI("10.5281/zenodo.16730669")
+pkg <- zenodo_to_gldp(z)
 
 
 
@@ -76,7 +47,7 @@ keyring::key_set_with_value("ZENODO_PAT", password = "{your_zenodo_token}")
 zenodo <- ZenodoManager$new(token = keyring::key_get(service = "ZENODO_PAT"))
 
 # Create a zenodo from data package
-z <- gldp2zenodoRecord(pkg)
+z <- gldp_to_zenodo(pkg)
 
 z <- zenodo$depositRecord(z, reserveDOI = TRUE, publish = FALSE)
 
@@ -93,8 +64,8 @@ for (f in list.files(pkg$version)) {
 # Update metadata from Zenodo
 # If you modify the metadata on zenodo, you can update your pkg with those information with
 
-z_updated <- zenodo$getDepositionByConceptDOI(z$getConceptDOI())
-pkg <- zenodoRecord2gldp(z_updated, pkg)
+z_updated <- zenodo$getDepositionByConceptDOI("10.5281/zenodo.16730669")
+pkg <- zenodo_2_gldp(z_updated, pkg)
 
 
 ## Make sure to submit to Zenodo community: https://zenodo.org/communities/geolocator-dp/
